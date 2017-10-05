8b46e2a5978bcdc26bafcbb9b47b2bd8cf2f2521
Maximo <max.sanchez.rios@hotmail.com>
2017-10-05 12:32:17 -0500

    Actualización del Proyecto, área de subida
    Creacion de la ventana de subida de archivos

---
+++ PGestal/js/menu.js
@@ -0,0 +1,39 @@
+<script type="text/javascript">
+var nav = false;
+function show_hide_nav(id){
+var navbar = document.getElementById(id);
+
+if(nav == false){
+navbar.style.display = "block";
+nav = true;
+}else{
+navbar.style.display = "none";
+nav = false;
+}
+}
+</script>
+(function() {
+
+  'use strict';
+
+  document.querySelector('.material-design-hamburger__icon').addEventListener(
+    'click',
+    function() {      
+      var child;
+      
+      document.body.classList.toggle('background--blur');
+      this.parentNode.nextElementSibling.classList.toggle('menu--on');
+
+      child = this.childNodes[1].classList;
+
+      if (child.contains('material-design-hamburger__icon--to-arrow')) {
+        child.remove('material-design-hamburger__icon--to-arrow');
+        child.add('material-design-hamburger__icon--from-arrow');
+      } else {
+        child.remove('material-design-hamburger__icon--from-arrow');
+        child.add('material-design-hamburger__icon--to-arrow');
+      }
+
+    });
+
+})();
